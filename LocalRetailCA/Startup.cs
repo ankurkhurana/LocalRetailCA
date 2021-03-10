c5b8411d9edbb98fac703faebcaec13d58525d63
AnkurKhurana <ankur_khurana@hotmail.com>
2021-03-09 21:50:48 -0500

    Main and Signup Page basic layout

--- LocalRetailCA/Startup.cs
+++ LocalRetailCA/Startup.cs
@@ -1,3 +1,4 @@
+using BlazorStrap;
 using LocalRetailCA.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
@@ -29,6 +30,7 @@ namespace LocalRetailCA
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBootstrapCss();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
